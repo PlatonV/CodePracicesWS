e23cf90d4da27284a39a9b8d08be8daebbc8b21a
= <=>
2015-11-13 12:30:53 +0200

    almost done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import re
 import log
+import sys
 from student import Student
 
 def getStudent(line):
@@ -10,6 +11,9 @@ def getStudent(line):
     line = re.sub(r"\s", "", line)
     # Generate fields list
     fields = line.split(';')
+    if len(fields) != 5:
+        raise ValueError("Invalid row found!")
+        return
     # Put in each student field the value from corresponding field element
     name = fields[0]
     surname = fields[1]
@@ -31,20 +35,38 @@ def run():
     Script entry point.
     """
     log.info("Trying to open csv file...")
-    with open("students.csv", 'r') as csvfile:
-        log.info("Succesfully opened csv file!")
-        # Iterate through each line in the csvfile
-        for line in csvfile:
-            # Get the student object from the current line string
-            log.info("Trying to create student...")
-            student = getStudent(line)
-            log.info("Student succesfully created! Result: " + str(student))
-            # If the student meets the requirements, print it
-            if checkStudent(student):
-                log.info("Student " + student.getName() + " checks.")
-                printStudent(student)
-            else:
-                log.info("Student " + student.getName() + " does not check.")
+    filename = "" 
+    try:
+        filename = sys.argv[1]
+    except:
+        print("Please provide csv file!")
+        return
+    try:
+        with open(filename, 'r') as csvfile:
+            log.info("Succesfully opened csv file!")
+            # Iterate through each line in the csvfile
+            row_count = 0
+            for line in csvfile:
+                # Get the student object from the current line string
+                log.info("Trying to create student...")
+                row_count += 1
+                try:
+                    student = getStudent(line)
+                    log.info("Student succesfully created! Result: " + str(student))
+                    # If the student meets the requirements, print it
+                    if checkStudent(student):
+                        log.info("Student " + student.getName() + " checks.")
+                        printStudent(student)
+                    else:
+                        log.info("Student " + student.getName() + " does not check.")
+                except ValueError as e:
+                    print(str(e) + " at row " + str(row_count))
+                    return
+                except:
+                    print("Error at row " + str(row_count) + "!")
+                    return
+    except:
+        print("Error openening file!")
 
 # Only call run() the program if it is run as a script
 if __name__ == "__main__":
